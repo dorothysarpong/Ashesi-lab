f25c105edb2a4a78c56a771fe5600463f61efffc
Samuel Agyemang <khermz2012@gmail.com>
2015-11-02 22:41:27 +0000

    Current studentProdutView file

--- studentProductView.php
+++ studentProductView.php
@@ -8,12 +8,13 @@
         <link type="text/css" rel="stylesheet" href="assets/css/modified.css" />
         <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </head>
 
-    <!--Import jQuery before materialize.js-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" src="assets/js/materialize.min.js"></script>
-    <script type="text/javascript" src="assets/js/script.js"></script>
+        <!--Import jQuery before materialize.js-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/materialize.min.js"></script>
+        <script type="text/javascript" src="assets/js/script.js"></script>
+
+    </head>
 
     <body>
         <ul id="dropdown1" class="dropdown-content">
@@ -30,6 +31,7 @@
                     </ul>
                 </div>
             </nav>
+
         </div>
 
         <div id="search">
@@ -45,14 +47,22 @@
                 </div>
             </nav>
         </div>
+
+        <div id="modals" class="modal modal-fixed-footer my_modal">
+
+<!--            <div class = "modal-footer">
+                <a id="but" href="#!" class="modal-action modal-close waves-effect waves-orange btn-flat ">Request</a>
+            </div>-->
+        </div>
+
     </body>
 
 </html>
 
 <?php
-include('mProduct.php');
+include_once('mProduct.php');
 
 $object = new Product();
-$object->student_view_product();//dwdw
+$object->student_view_product(); //dwdw
 
 
